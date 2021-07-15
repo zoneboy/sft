1d50ac9b8fde6ff64fc568c382635b9ed44cf8f5
zoneboy <lateef4ril32@gmail.com>
2021-07-15 20:50:57 +0100

    Reverted

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,10 +15,10 @@ $(function() {
         var scroll = $(window).scrollTop();
         if (scroll < 20) {
             $(".navbar-area").removeClass("sticky");
-            $(".navbar-area img").attr("src", "assets/images/logo100bg.png");
+            $(".navbar-area img").attr("src", "assets/images/logo.svg");
         } else {
             $(".navbar-area").addClass("sticky");
-            $(".navbar-area img").attr("src", "assets/images/logo100bg.png");
+            $(".navbar-area img").attr("src", "assets/images/logo-2.svg");
         }
     });
 
